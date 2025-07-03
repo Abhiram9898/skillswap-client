179af98d70c8647dd6c311833ecb538899cdecfd
Abhiram9898 <abhiramkumar2000@gmail.com>
2025-07-04 04:12:57 +0530

    updated axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
+// src/utils/axios.js
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // now works in production too
+  baseURL: '/api', // thanks to Vite proxy
   withCredentials: true,
 });
 
