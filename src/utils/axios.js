465937ec8548b7adfa5d82183cb86f83608a190c
Abhiram9898 <abhiramkumar2000@gmail.com>
2025-07-04 06:13:03 +0530

    update axios file

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,18 @@
-// src/utils/axios.js
+// // src/utils/axios.js
+// import axios from 'axios';
+
+// const instance = axios.create({
+//   baseURL: '/api', // thanks to Vite proxy
+//   withCredentials: true,
+// });
+
+// export default instance;
+
+
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: '/api', // thanks to Vite proxy
+  baseURL: import.meta.env.VITE_API_URL, // uses full URL from env
   withCredentials: true,
 });
 
